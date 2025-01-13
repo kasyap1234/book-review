66a014d3e03bdec9d684ef7e54c6b968a048ce04
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-01-13 21:13:19 +0530

    need to modify review service to remove userid

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main 
+
+
+import (
+ "kasyap1234/book-review-recommendation/client"
+"context"
+"log"
+)
+
+
+func main(){
+	c,err :=client.NewClient(); 
+	if err !=nil {
+		log.Fatal(err); 
+	}
+
+	defer c.Close(); 
+	registerResponse,err :=c.Register("kasyap1","password")
+log.Println(registerResponse)
+bookResp, err :=c.AddBook("harry potter","Jk Rowling"); 
+log.Println(bookResp)
+
+}
