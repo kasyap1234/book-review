3cda5ee5df9dd10e21605372c8f8a51365165859
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-01-13 21:14:55 +0530

    modify

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
-package main 
-
+package main
 
 import (
- "kasyap1234/book-review-recommendation/client"
-"context"
-"log"
+	"kasyap1234/book-review-recommendation/client"
+
+	"log"
 )
 
-
-func main(){
-	c,err :=client.NewClient(); 
-	if err !=nil {
-		log.Fatal(err); 
+func main() {
+	c, err := client.NewClient()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	defer c.Close(); 
-	registerResponse,err :=c.Register("kasyap1","password")
-log.Println(registerResponse)
-bookResp, err :=c.AddBook("harry potter","Jk Rowling"); 
-log.Println(bookResp)
+	defer c.Close()
+
+	registerResponse, err := c.Register("kasyap1", "password")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(registerResponse)
+	bookResp, err := c.AddBook("harry potter", "Jk Rowling")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(bookResp)
 
 }
